996180ea7d2d98e0e2310fddebcc3127b69355b6
Gianlucampos <gianlucampos97@gmail.com>
2021-10-12 00:31:03 -0300

    Banco de dados H2 e criação automática da base de dados

---
+++ src/main/java/com/nelioalves/cursomc/domain/Categoria.java
@@ -0,0 +1,53 @@
+package com.nelioalves.cursomc.domain;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@SuppressWarnings("PersistenceUnitPresent")
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+    public Categoria() {
+    }
+
+    public Categoria(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
---
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,22 @@
+package com.nelioalves.cursomc.resources;
+
+import com.nelioalves.cursomc.domain.Categoria;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Categoria> listar() {
+        Categoria cat1 = new Categoria(1L, "Informática");
+        Categoria cat2 = new Categoria(2L, "Informática");
+        return Arrays.asList(cat1, cat2);
+    }
+
+}
