033465949c6386f4b373e73b0abced08e39c88d6
gianlucampos <gianlucampos97@gmail.com>
2023-12-03 13:52:10 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -1,5 +1,7 @@
 package com.nelioalves.cursomc;
 
+import com.nelioalves.cursomc.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
+    @Autowired
+    private S3Service s3Service;
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
@@ -15,6 +19,6 @@ public class CursomcApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-    
+        s3Service.uploadFile("/home/gianlucampos/Downloads/imagens/cp1.jpg");
     }
 }
